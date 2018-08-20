9d5cf702915871d3d373f7ce609959f5e7645e0c
Yan Fei <fei.yan@nokia-sbell.com>
2018-08-20 21:52:59 +0800

    refactor complex search function

--- src/checkers/search.rs
+++ src/checkers/search.rs
@@ -1,6 +1,8 @@
 
 extern crate hyper;
 extern crate tokio_core;
+use std::sync::mpsc::Sender;
+use std::sync::mpsc::Receiver;
 use std::clone::Clone;
 use std::sync::mpsc::channel;
 use self::tokio_core::reactor::Core;
@@ -21,62 +23,91 @@ pub fn perform_gen<T>(core: &mut Core, fetcher: &mut Fetcher, uri: &str, jql: &s
         where T: DeserializeOwned + Clone {
     //construct search
     let search = Query::new(jql.to_string(), 100, fields);
-    let mut first_job = vec![search.clone()];
-    search_and_collect(result, &mut first_job, core, fetcher, &uri);
+    let mut searcher = Searcher::new(core, fetcher, uri, vec![search.clone()]);
+    searcher.perform(result);
     if result.issues.len() == 0 {
         error!("First search failed?");
         panic!("Unexpected ending!");
     }
 
     info!("Got first result now, check remaining by page info!");
-    //search remaining and combine the result
-    let mut pending_jobs = get_remaining_queries(result, &search);
-    search_and_collect(result, &mut pending_jobs, core, fetcher, &uri);
+    searcher.reset_pending(search.create_remaining(result.total))
+            .perform(result);
 }
 
-//run given queries repeatedly until all results got
-fn search_and_collect<T:DeserializeOwned>(result: &mut QueryResult<T>, pending_jobs: &mut Vec<Query>, 
-        core: &mut Core, fetcher: &mut Fetcher, uri: &str){
-    let (guard_tx, guard_rx) = channel();
+struct Searcher<'a> {
+    core:&'a mut Core,
+    fetcher: &'a mut Fetcher,
+    uri: &'a str,
+    pending_jobs: Vec<Query>,
+    finished: Vec<usize>,
+}
 
-    while pending_jobs.len() > 0 {
+impl <'a> Searcher<'a> {
+    fn new(core: &'a mut Core, fetcher: &'a mut Fetcher, uri: &'a str, 
+            pending: Vec<Query>) -> Searcher<'a> {
+        Searcher{
+            core: core,
+            fetcher: fetcher,
+            uri: uri,
+            pending_jobs: pending,
+            finished: Vec::new(),
+        }
+    }
+
+    fn reset_pending(&mut self, new_pending: Vec<Query>) -> &mut Searcher<'a> {
+        self.pending_jobs = new_pending;
+        self
+    }
+
+    fn perform<T: DeserializeOwned>(&mut self, result: &mut QueryResult<T>){
+        let (tx, rx) = channel();
+        while self.pending_jobs.len() > 0 {
+            self.finished.clear();
+            self.drain_all_jobs(&tx);
+            self.collect_all_responses(result, &rx);
+            self.clean_finished_from_pending();
+        }
+    }
+
+    fn drain_all_jobs<T: DeserializeOwned>(&mut self, 
+            sender: &Sender<Result<Box<QueryResult<T>>, usize>>) {
         let mut sub_queries = Vec::new();
-
         //Drain all pending jobs 
-        for qry in pending_jobs.iter() {
+        for qry in self.pending_jobs.iter() {
             //query this page
-            let my_job = qry.startAt;
-            let my_guard = guard_tx.clone();
+            let sender1 = sender.clone();
             let parser = move |json: &str, code: StatusCode| {
                 let my_result = match code{
-                    StatusCode::Ok => parse_query_result::<T>(&json).ok_or_else(|| my_job),
-                    _ => Err(my_job),
+                    StatusCode::Ok => parse_query_result::<T>(&json).ok_or_else(|| qry.startAt),
+                    _ => Err(qry.startAt),
                 };
-                let _x = my_guard.send(my_result);
+                let _x = sender1.send(my_result);
             };
 
-            let post_info = RequestInfo::post(uri, &qry.to_json().unwrap());
-            let job1 = qry.startAt;
-            let guard1 = guard_tx.clone();
-            let sub_fetch = fetcher.query_with(post_info, core, Some(parser))
+            let post_info = RequestInfo::post(self.uri, &qry.to_json().unwrap());
+            let guard1 = sender.clone();
+            let sub_fetch = self.fetcher.query_with(post_info, self.core, Some(parser))
                     .map_err(move |err| { 
                         //TODO: handle exceptions in graceful manner?
-                        warn!("This job {} has failed by {}", job1, err);
-                        let _x = guard1.send(Err(job1 + 10000)); 
+                        warn!("This job {} has failed by {}", qry.startAt, err);
+                        let _x = guard1.send(Err(qry.startAt + 10000)); 
                         "failed"
                     });
             sub_queries.push(sub_fetch);
         }
-        let _x = core.run(join_all(sub_queries));
+        let _x = self.core.run(join_all(sub_queries));
+    }
 
-        //collect paged sub-queries
-        let mut finished: Vec<usize> = Vec::new();
-        let total = pending_jobs.len();
+    fn collect_all_responses<T:DeserializeOwned>(&mut self, result: &mut QueryResult<T>, 
+        receiver: &Receiver<Result<Box<QueryResult<T>>, usize>>) {
+    
+        let total = self.pending_jobs.len();
         for  x in 1..(total+1) {
-            if let Ok(process_result) = guard_rx.recv() {
+            if let Ok(process_result) = receiver.recv() {
                 match process_result {
                     Ok(qry) => {
-                        finished.push(qry.startAt);
+                        self.finished.push(qry.startAt);
                         result.collect_from(*qry);
                         info!("[{}/{}] Collected a paged response!", x, total);
                     },
@@ -95,17 +126,13 @@ fn search_and_collect<T:DeserializeOwned>(result: &mut QueryResult<T>, pending_j
                 error!("recev error now!");
             }
         }
+    }
 
-        //check failures and retain them for retry
-        pending_jobs.retain(|ref qry| {
+    fn clean_finished_from_pending(&mut self) {
+        let ref mut finished = self.finished;
+        self.pending_jobs.retain(|ref qry| {
             finished.sort_unstable();
             finished.binary_search(&qry.startAt).is_err()
         });
     }
-} 
-
-//Create remaining queries list based on first paged search result
-fn get_remaining_queries<T>(qry_result: &QueryResult<T>, search: &Query) -> Vec<Query>{
-    info!("Got issues = {}, total = {}", qry_result.issues.len(), qry_result.total);
-    search.create_remaining(qry_result.total)
 }
